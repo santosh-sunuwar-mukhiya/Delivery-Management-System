c5032dd2ac24848c2095882f513db2ac2909ac40
santosh-sunuwar-mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-03-30 00:43:10 +0545

    seller-service: tighten token generation flow and standardize auth error handling

--- app/api/routers/seller.py
+++ app/api/routers/seller.py
@@ -1,35 +1,27 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
-import jwt
 
-from app.database.models import Seller
-from app.utils import decode_access_token
-
-from ..dependencies import SellerServiceDep, SessionDep
+from ..dependencies import SellerServiceDep
 from ..schemas.seller import SellerCreate, SellerRead
-
-from app.core.security import oauth2_scheme
 
 router = APIRouter(prefix="/seller", tags=["Seller"])
 
 
 ### Register a seller
 @router.post("/signup", response_model=SellerRead)
-async def register_seller(
-    seller: SellerCreate,
-    service: SellerServiceDep
-):
+async def register_seller(seller: SellerCreate, service: SellerServiceDep):
     return await service.add(seller)
 
 
+### Login the seller
 @router.post("/token")
 async def login_seller(
     request_form: Annotated[OAuth2PasswordRequestForm, Depends()],
     service: SellerServiceDep,
 ):
-    token = await service.token(request_form.username, request_form.password)  # type: ignore
+    token = await service.token(request_form.username, request_form.password)
     return {
         "access_token": token,
         "type": "jwt",
