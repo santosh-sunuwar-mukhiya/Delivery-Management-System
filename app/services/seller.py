8ed5b45f2a7006a96a35c0c3976025736f8a5550
santosh-sunuwar-mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-04-06 20:29:25 +0545

    feat: implement seller service with registration and authentication

--- app/services/seller.py
+++ app/services/seller.py
@@ -1,55 +1,19 @@
-from fastapi import HTTPException, status
-from passlib.context import CryptContext
-
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.schemas.seller import SellerCreate
 from app.database.models import Seller
-from app.utils import generate_access_token
 
-password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from .user import UserService
 
-class SellerService:
+
+class SellerService(UserService):
     def __init__(self, session: AsyncSession):
-        # Get database session to perform database operations
-        self.session = session
+        super().__init__(Seller, session)
 
-    async def add(self, credentials: SellerCreate) -> Seller:
-        seller = Seller(
-            **credentials.model_dump(exclude=["password"]),  # type: ignore
-            # Hashed password
-            password_hash=password_context.hash(credentials.password),
+    async def add(self, seller_create: SellerCreate) -> Seller:
+        return await self._add_user(
+            seller_create.model_dump()
         )
-        self.session.add(seller)
-        await self.session.commit()
-        await self.session.refresh(seller)
-
-        return seller
 
     async def token(self, email, password) -> str:
-        # Validate the credentials
-        result = await self.session.execute(
-            select(Seller).where(Seller.email == email)
-        ) 
-        seller = result.scalar()
-
-        if seller is None or not password_context.verify(
-            password,
-            seller.password_hash,
-        ):
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Email or password is incorrect",
-            )
-
-        token = generate_access_token(
-            data={
-                "user": {
-                    "name": seller.name,
-                    "id": str(seller.id),
-                }
-            }
-        )
-
-        return token
+        return await self._generate_token(email, password)
