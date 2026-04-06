ea33b161051b895b1bc475cd123d1ce839b8dafc
santosh-sunuwar-mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-04-06 20:29:25 +0545

    feat: add seller endpoints (signup, login, logout)

--- app/api/routers/seller.py
+++ app/api/routers/seller.py
@@ -5,32 +5,37 @@ from fastapi.security import OAuth2PasswordRequestForm
 
 from app.database.redis import add_jti_to_blacklist
 
-from ..dependencies import SellerServiceDep, get_access_token
+from ..dependencies import SellerServiceDep, get_seller_access_token
 from ..schemas.seller import SellerCreate, SellerRead
 
 router = APIRouter(prefix="/seller", tags=["Seller"])
 
 
-### Register a seller
+### Register a new seller
 @router.post("/signup", response_model=SellerRead)
 async def register_seller(seller: SellerCreate, service: SellerServiceDep):
     return await service.add(seller)
 
 
-### Login the seller
+### Login a seller
 @router.post("/token")
 async def login_seller(
     request_form: Annotated[OAuth2PasswordRequestForm, Depends()],
     service: SellerServiceDep,
 ):
     token = await service.token(request_form.username, request_form.password)
-    return {"access_token": token, "token_type": "bearer"}
+    return {
+        "access_token": token,
+        "type": "jwt",
+    }
 
 
 ### Logout a seller
 @router.get("/logout")
 async def logout_seller(
-    token_data: Annotated[dict, Depends(get_access_token)],
+    token_data: Annotated[dict, Depends(get_seller_access_token)],
 ):
     await add_jti_to_blacklist(token_data["jti"])
-    return {"detail": "Successfully logged out"}
+    return {
+        "detail": "Successfully logged out"
+    }
