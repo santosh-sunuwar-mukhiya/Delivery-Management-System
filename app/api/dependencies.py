6f208132cc2297ccd537f13d3d68dda0c0afdf26
santosh-sunuwar-mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-04-06 20:29:25 +0545

    feat: add dependency injection for authentication and services

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -4,10 +4,11 @@ from uuid import UUID
 from fastapi import Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core.security import oauth2_scheme
-from app.database.models import Seller
+from app.core.security import oauth2_scheme_seller, oauth2_scheme_partner
+from app.database.models import DeliveryPartner, Seller
 from app.database.redis import is_jti_blacklisted
 from app.database.session import get_session
+from app.services.delivery_partner import DeliveryPartnerService
 from app.services.seller import SellerService
 from app.services.shipment import ShipmentService
 from app.utils import decode_access_token
@@ -18,12 +19,11 @@ SessionDep = Annotated[AsyncSession, Depends(get_session)]
 
 
 # Access token data dep
-async def get_access_token(
-    token: Annotated[str, Depends(oauth2_scheme)],
-) -> dict:
+async def _get_access_token(token: str) -> dict:
     data = decode_access_token(token)
 
-    if data is None or await is_jti_blacklisted(data["jti"]):  # type: ignore
+    # Validate the token
+    if data is None or await is_jti_blacklisted(data["jti"]):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid or expired access token",
@@ -32,17 +32,64 @@ async def get_access_token(
     return data
 
 
+# Seller access token data
+async def get_seller_access_token(
+    token: Annotated[str, Depends(oauth2_scheme_seller)],
+) -> dict:
+    return await _get_access_token(token)
+
+
+# Delivery partner access token data
+async def get_partner_access_token(
+    token: Annotated[str, Depends(oauth2_scheme_partner)],
+) -> dict:
+    return await _get_access_token(token)
+
+
 # Logged In Seller
 async def get_current_seller(
-    token_data: Annotated[dict, Depends(get_access_token)],
+    token_data: Annotated[dict, Depends(get_seller_access_token)],
     session: SessionDep,
 ):
-    return await session.get(Seller, UUID(token_data["user"]["id"]))
+    seller = await session.get(
+        Seller,
+        UUID(token_data["user"]["id"]),
+    )
+
+    if seller is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not authorized",
+        )
+
+    return seller
+
+
+# Logged In Delivery partner
+async def get_current_partner(
+    token_data: Annotated[dict, Depends(get_partner_access_token)],
+    session: SessionDep,
+):
+    partner = await session.get(
+        DeliveryPartner,
+        UUID(token_data["user"]["id"]),
+    )
+
+    if partner is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not authorized",
+        )
+
+    return partner
 
 
 # Shipment service dep
 def get_shipment_service(session: SessionDep):
-    return ShipmentService(session)
+    return ShipmentService(
+        session,
+        DeliveryPartnerService(session),
+    )
 
 
 # Seller service dep
@@ -50,10 +97,21 @@ def get_seller_service(session: SessionDep):
     return SellerService(session)
 
 
-# Seller dep
+# Delivery partner service dep
+def get_delivery_partner_service(session: SessionDep):
+    return DeliveryPartnerService(session)
+
+
+# Seller dep annotation
 SellerDep = Annotated[
     Seller,
     Depends(get_current_seller),
+]
+
+# Delivery partner dep annotation
+DeliveryPartnerDep = Annotated[
+    DeliveryPartner,
+    Depends(get_current_partner),
 ]
 
 # Shipment service dep annotation
@@ -66,4 +124,10 @@ ShipmentServiceDep = Annotated[
 SellerServiceDep = Annotated[
     SellerService,
     Depends(get_seller_service),
+]
+
+# Delivery partner service dep annotaion
+DeliveryPartnerServiceDep = Annotated[
+    DeliveryPartnerService,
+    Depends(get_delivery_partner_service),
 ]
